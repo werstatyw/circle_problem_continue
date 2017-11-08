eb9658b5b97b6a4c3cc0029222d496c47fefb717
hallgató <hallgat@LABOR115-17>
2017-11-08 13:09:56 +0100

    Add project files.

---
+++ circle_problem_continue/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_first_project
+{
+    class Circle
+    {
+        double x = 1; //origin x coord
+//        public int X { get; set; }
+public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+double y = 2; //origin y coord
+        public int Y { get; set; }
+        double r = 10; //radius
+        public int R { get; set; }
+        public Circle(double newx, double newy, double newr)
+        {
+            x = newx; y = newy; r = newr;
+        }
+        public string print()
+        {
+            return "0(" + x + "," + y + ") r =" + r;
+        }
+        public bool inside(double x1, double y1)
+        {
+            return (x - x1) * (x - x1) + (y - y1) * (y - y1) < r * r;
+        }
+        public  double measuredist(double x1, double y1)
+        {
+            return (x - x1) * (x - x1) + (y - y1) * (y - y1);
+        }
+    }
+}
---
+++ circle_problem_continue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_first_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(1, 2, 10);
+            Circle c2 = new Circle(3, 4, 15);
+            Console.WriteLine(c1.print());
+            Console.WriteLine("(2,4) " + c1.inside(2, 4) + " " + c2.inside(2, 4));
+            c1.X = 1.2;
+            Console.WriteLine(c1.X);
+            Console.WriteLine(c2.print());
+            Console.WriteLine("(1,2) " + c1.inside(1, 2) + " " + c2.inside(1, 2));
+            Console.WriteLine(c1.X);
+            Console.WriteLine("distance is " + c1.measuredist(1, 2) + " " + c2.measuredist(1, 2));
+            Console.WriteLine("The end.");
+            Console.ReadKey();
+        }
+    }
+}
